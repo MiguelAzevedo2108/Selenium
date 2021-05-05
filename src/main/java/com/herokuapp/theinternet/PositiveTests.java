8309916382baf34f1229bcf6e0abfb425896c761
Miguel Azevedo <40613610+MiguelAzevedo2108@users.noreply.github.com>
2021-05-05 19:28:35 +0100

    loginTest

---
+++ src/main/java/com/herokuapp/theinternet/PositiveTests.java
@@ -0,0 +1,19 @@
+package com.herokuapp.theinternet;
+
+public class PositiveTests {
+
+    public void loginTest(){
+        //Create driver
+//        open test page
+//        enter username
+//        enter password
+//        click login button
+//        verifications:
+//        new url
+//        logout button is visible
+//        succesful login message
+
+
+
+    }
+}

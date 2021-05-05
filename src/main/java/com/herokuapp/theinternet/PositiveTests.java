9d86ddde8b686cc966b548a94899624730dfc32f
Miguel Azevedo <40613610+MiguelAzevedo2108@users.noreply.github.com>
2021-05-05 19:57:23 +0100

    loginTest in construction

--- src/main/java/com/herokuapp/theinternet/PositiveTests.java
+++ src/main/java/com/herokuapp/theinternet/PositiveTests.java
@@ -1,19 +1,55 @@
 package com.herokuapp.theinternet;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+//Using TestNG framework. There are some feature in TestNG that are not available in JUnit
 public class PositiveTests {
 
+    @Test
     public void loginTest(){
+        System.out.println("Starting..");
+
         //Create driver
-//        open test page
-//        enter username
-//        enter password
-//        click login button
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        //sleep for 3 seconds just to slow down
+        sleep(3000);
+
+        //maximize browser window
+        driver.manage().window().maximize();
+
+        //open test page
+        String url = "http://the-internet.herokuapp.com/login";
+        driver.get(url);
+        System.out.println("Page is opened");
+
+        //enter username
+
+
+        //enter password
+        //click login button
+
+
 //        verifications:
 //        new url
 //        logout button is visible
 //        succesful login message
 
+        //Close Browser
+        driver.quit();
 
+    }
 
+    private void sleep(long ms) {
+        //sleep slow down
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
